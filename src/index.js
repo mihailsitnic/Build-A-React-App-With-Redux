938c4ecf860d69c942c188450cac9f8b52d3a666
Mihail Sitnic <mihail.sitnic@gmail.com>
2017-12-12 20:03:00 +0200

    06-egghead-react-create-a-redux-store

--- src/index.js
+++ src/index.js
@@ -3,14 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import store from './store';
 
-const state = {
-  todos: [
-    {id: 1, name: 'Render static UI', isComplete: true},
-    {id: 2, name: 'Create Initial state', isComplete: false},
-    {id: 3, name: 'Render based on state', isComplete: false}
-  ]
-}
+const state = store.getState()
 
-ReactDOM.render(<App todos={state.todos} />, document.getElementById('root'));
+ReactDOM.render(<App {...state} />, document.getElementById('root'));
 registerServiceWorker();
--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,12 +1,15 @@
 const initState = {
-  todos: []
+  todos: [
+    {id: 1, name: 'Render static UI', isComplete: true},
+    {id: 2, name: 'Create Initial state', isComplete: false},
+    {id: 3, name: 'Render based on state', isComplete: false}
+  ]
 }
 
 export default (state = initState, action) => {
   switch (action.type) {
     case 'TODO_ADD':
-      return {... state, todos: state.todos.concat(action.payload)}
-
+      return { ...state, todos: state.todos.concat(action.payload) }
     default:
       return state
   }
