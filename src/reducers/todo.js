d51e6d8925047c519a86412666c8685786c6a947
Mihail Sitnic <mihail.sitnic@gmail.com>
2017-12-12 20:40:48 +0200

    08-egghead-react-keep-ui-and-state-synchronized-using-controlled-components-in-react-with-redux

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -3,13 +3,16 @@ const initState = {
     {id: 1, name: 'Render static UI', isComplete: true},
     {id: 2, name: 'Create Initial state', isComplete: false},
     {id: 3, name: 'Render based on state', isComplete: false}
-  ]
+  ],
+  currentTodo: ''
 }
 
 export default (state = initState, action) => {
   switch (action.type) {
     case 'TODO_ADD':
       return { ...state, todos: state.todos.concat(action.payload) }
+    case 'CURRENT_UPDATE':
+      return {...state, currentTodo: action.payload}
     default:
       return state
   }
