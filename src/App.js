fd53cfbc97688345de6d3fc7aa0bb0665dd33a7a
Mihail Sitnic <mihail.sitnic@gmail.com>
2017-12-12 16:47:13 +0200

    03-egghead-react-render-in-react-based-on-a-global-state-object

--- src/App.js
+++ src/App.js
@@ -10,6 +10,21 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React width Redux</h1>
         </header>
+        <div className="Todo-App">
+          <form>
+            <input type="text" />
+          </form>
+        </div>
+        <div className="Todo-List">
+          <ul>
+            {this.props.todos.map(todo => (
+              <li key={todo.id}>
+                <input type="checkbox" defaultChecked={todo.isComplete} />
+                {todo.name}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }
